62118024fead6f86323f0f1bf24444c17df053f8
Crista Cooper <aine3d@gmail.com>
2017-10-14 16:55:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,7 +4,9 @@ def fizzbuzz(int)
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
-  else int % 3 == 0 && int % 5 == 0
+  elsif int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
+  else 
+    "nil"
   end
 end
